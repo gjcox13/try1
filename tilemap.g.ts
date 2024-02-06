ea510e67a1703b64d48b56c4c7c515498cd68eb0
gjcox13 <159101312+gjcox13@users.noreply.github.com>
2024-02-05 22:08:23 -0600

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100002020202020202020202020202020202000000000000000000000002000200020200020202020200020202020002000202000000020002000200000000020002020002020200020002000202020200020200000000000200000000000200000202000202020202020202020002000202020000020002000000000200020000020202000000000002020002000202000202020202020202020000020000000002020000000000000000020202020202020200020202000202020200000000000202000200020000000000000202020202020002000200020202020202000000020200000002000000000000000002010202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.chestClosed,sprites.dungeon.floorLight0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
